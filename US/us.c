3052963b787f4db5bb6dd7b0d09941b3421edef7
saleee <gabor.salamon@unicomp.hu>
2013-12-16 20:52:47 +0100

    test code if compiles with sleep

--- US/us.c
+++ US/us.c
@@ -1,51 +1,36 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 
 
-#define ADC_ADDR 0x48
-#define ADC_PIN 1
+#define US_TRIGGER_PIN	7
+#define US_ECHO_PIN	6
 
 int main()
 {
-	int adcResource;
-	int adcValue[4];
-
 	//init the wiringPi
 	wiringPiSetup();
 
-	//set up to input
-	pinMode(ADC_PIN, INPUT);
-	//status is off
-	pullUpDnControl(ADC_PIN, PUD_OFF);
+	//set up pin directions
+	pinMode(US_TRIGGER_PIN, OUTPUT);
+	pinMode(US_ECHO_PIN, INPUT);
+	digitalWrite(US_TRIGGER_PIN, 0);
+	
+	//set up pin pullups
+	pullUpDnControl(US_TRIGGER_PIN, PUD_OFF);
+	pullUpDnControl(US_ECHO_PIN, PUD_OFF);
 	
 	printf("\nGPIO config done.\r\n\r\n");
 	
-	//Init I2C. Error if the adcResource is minus
-	wiringPiI2CSetup(ADC_ADDR);
+	sleep(1000);
+	
+	printf("\nULTRASONIC MEASURE.\r\n\r\n");
+	
 
-		//the first register is the 
-		adcValue[0] = wiringPiI2CReadReg8(adcResource, 0);
-		adcValue[1] = wiringPiI2CReadReg16(adcResource, 1);
-		
-		printf("\nADC value 0: 0x%i \r\n", adcValue[0]);
-		printf("\nADC value 1: %i \r\n", adcValue[1]);
-
-		fflush(stdout);
+	fflush(stdout);
 
 
 }
-/*
-adc_config()
-* 18. oldal szerinti beállítás
-* alert bit polaritás beállítás
-* 
-* 
-* bit sorrendre figyelni kell
-* %i nem hexa
-* Javaból jni-vel hogy lehet használni a wiringPi-t
-
-
-*/
