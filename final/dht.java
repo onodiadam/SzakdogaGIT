426bc863311ceb097b62bfb05d7795e92b7e24c8
saleee <gabor.salamon@unicomp.hu>
2013-12-28 23:34:18 +0100

    fix java shift

--- final/dht.java
+++ final/dht.java
@@ -14,6 +14,6 @@ class DhtMain
 	{
 		Dht humtemp = new Dht();
 		long ht = humtemp.readDHT();
-		System.out.println("Ht: " + ht + "\r\n");
+		System.out.println("H: " + (int)((ht & 0xFFFF0000) >> 16) + "T:" + (int)(ht & 0x0000FFFF) + "\r\n");
 	}
 }
